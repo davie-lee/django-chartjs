622a70e695c37a1b8c25949acc1f901a4cda0d0c
davie-lee <daviehslee@gmail.com>
2021-08-18 10:42:51 -0400

    Chartjs added

--- dataviz/models.py
+++ dataviz/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=30)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    population = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.name}, {self.country}"
